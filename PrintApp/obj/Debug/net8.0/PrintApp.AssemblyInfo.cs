21623b3240dda29c3c02db371df16c36caa21249
minhchien137 <bmc13072001@gmail.com>
2026-08-04 08:29:30 +0700

    conti

--- PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
+++ PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d155d075c029d732178ccc04e083d337b5c28085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3548da498a19ab60229f70a876f5b9de9eba538b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
