0d2bb1883c21bc1121fb6d10474390ca5e11ab79
minhchien137 <bmc13072001@gmail.com>
2026-08-05 13:38:40 +0700

    Check không cho Repack nếu Serial Number đó đã được gỡ unbind ra nhưng mà chưa rework thì không thể Repack được

--- PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
+++ PrintApp/obj/Debug/net8.0/PrintApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d0bd624afc610cf14a805e3438f82de44b859e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44edad758a0a4a98a46e4db7267f7e3f7783457d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrintApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
